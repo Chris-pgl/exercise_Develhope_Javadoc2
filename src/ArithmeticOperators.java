f9a9e9c69fae79b99cc5a19bb851330213d8fc60
Chris <cr1s@hotmail.it>
2022-12-11 10:43:27 +0100

    Add: Exercise_Javado2

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,46 @@
+public class ArithmeticOperators {
+    /**
+     * Method summation
+     * @param num1
+     * @param num2
+     * @return  the result sum
+     */
+    public int sum(int num1, int num2){
+        return num1 + num2;
+    }
+
+
+    /**
+     * Method subtraction
+     * @param num1
+     * @param num2
+     * @return result sub
+     */
+    public int sub(int num1, int num2){
+        return num1 - num2;
+    }
+
+
+    /**
+     * Method multiplication
+     * @param num1
+     * @param num2
+     * @return result mul
+     */
+    public int mul(int num1, int num2){
+        return num1 * num2;
+    }
+
+
+    /**
+     * method division
+     * @param num1
+     * @param num2
+     * @return result div
+     */
+    public int div(int num1, int num2){
+        return num1 / num2;
+    }
+
+
+}
---
+++ src/Tester.java
@@ -0,0 +1,36 @@
+
+public class Tester {
+    public static void main(String[] args) {
+
+        /**
+         * creation Object
+         */
+        ArithmeticOperators operation = new ArithmeticOperators();
+
+
+        /**
+         * invoke sum in println and add number
+         */
+        System.out.println("sum: " + operation.sum(1,2));
+
+        /**
+         * invoke sub in println and add number
+         */
+        System.out.println("sub: " +operation.sub(4,3));
+
+        /**
+         * invoke mul in println and add number
+         */
+        System.out.println("mul: " +operation.mul(2,5));
+
+        /**
+         * invoke div in println and add number
+         */
+        System.out.println("div: " +operation.div(8,2));
+
+
+
+
+
+    }
+}
